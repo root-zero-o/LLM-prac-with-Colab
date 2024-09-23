c8bfb508c50ed90884eb404d804be96a31626965
root-zero-o <rootzero17@gmail.com>
2024-09-23 22:37:58 +0900

    2.5

--- 2장/transformer-architecture.py
+++ 2장/transformer-architecture.py
@@ -161,3 +161,40 @@ class PreLayerNormFeedForward(nn.Module):
     x = x + self.linear2(self.dropout1(self.activation(self.linear1(x))))
     x = self.dropout2(x)
     return x
+
+# 7) 인코더 
+# 인코더 층
+class TransformerEncoderLayer(nn.Module):
+  def __init__(self, d_model, nhead, dim_feedforward, dropout):
+    super().__init__()
+    self.attn = MultiheadAttention(d_model, d_model, nhead) # 멀티 헤드 어텐션 클래스
+    self.norm1 = nn.LayerNorm(d_model) # 층 정규화
+    self.dropout1 = nn.Dropout(dropout) # 드롭아웃
+    self.feed_forward = PreLayerNormFeedForward(d_model, dim_feedforward, dropout) # 피드 포워드
+
+  def forward(self, src):
+    norm_x = self.norm1(src)
+    attn_output = self.attn(norm_x, norm_x, norm_x)
+    x = src + self.dropout1(attn_output) # 잔차 연결
+  
+    # 피드 포워드
+    x = self.feed_forward(x)
+    return x
+
+# 인코더 구현
+import copy
+def get_clones(module, N):
+  return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+class TransformerEncoder(nn.Module):
+  def __init__(self, encoder_layer, num_layers):
+    super().__init__()
+    self.layers = get_clones(encoder_layer, num_layers)
+    self.num_layers = num_layers
+    self.norm = norm
+  
+  def forward(self, src):
+    output = src
+    for mod in self.layers:
+      output = mod(output)
+    return output
