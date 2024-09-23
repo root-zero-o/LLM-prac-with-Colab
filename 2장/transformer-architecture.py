f8ca8fde72d85e21cd6dbb5e2a170030f4167379
root-zero-o <rootzero17@gmail.com>
2024-09-23 22:54:01 +0900

    2.6

--- 2장/transformer-architecture.py
+++ 2장/transformer-architecture.py
@@ -198,3 +198,67 @@ class TransformerEncoder(nn.Module):
     for mod in self.layers:
       output = mod(output)
     return output
+
+# 8) 디코더
+# - 이전에 생성된 토큰까지만 확인해야 한다 -> 마스크 연산 추가
+
+# 마스크 어텐션
+def compute_attention(querys, keys, values, is_causal=False):
+  dim_k = querys.size(-1)
+  scores = querys @ keys.transpose(-2, -1) / sqrt(dim_k)
+
+  # 추가된 부분
+  if is_causal:
+    query_length = querys.size(2)
+    key_length = keys.size(2)
+    temp_mask = torch.ones(query_length, key_length, dtype=torch.bool).tril(diagonal=0)
+    scores = scores.masked_fill(temp_mask == False, float("-inf"))
+
+  weights = F.softmax(scores, dim=-1)
+  return weights @ values
+
+# 크로스 어텐션
+# - 쿼리 : 디코더의 잠재 상태를 사용 / 키, 값 : 인코더의 결과 사용
+
+# 크로스 어텐션 포함된 디코더 층
+class TransformerDecoderLayer(nn.Module):
+  def __init__(self, d_model, nhead, dim_feedforward=2048, dropout=0.1):
+    super().__init__()
+    self.self_attn = MultiheadAttention(d_model, d_model, nhead)
+    self.multihead_attn = MultiheadAttention(d_model, d_model, nhead)
+    self.feed_forward = PreLayerNormFeedForward(d_model, dim_feedforward, dropout)
+
+    self.norm1 = nn.LayerNorm(d_model)
+    self.norm2 = nn.LayerNorm(d_model)
+    self.dropout1 = nn.Dropout(dropout)
+    self.dropout2 = nn.Dropout(dropout)
+
+  def forward(self, tgt, encoder_output, is_causal=True):
+    # 셀프 어텐션 연산
+    x = self.norm1(tgt)
+    x = x + self.dropout1(self.self_attn(x, x, x))
+
+    # 크로스 어텐션 연산
+    x = self.norm2(x)
+    x = x + self.dropout2(self.multihead_attn(x, encoder_output, encoder_output, is_causal=is_causal))
+
+    # 피드 포워드 연산
+    x = self.feed_forward(x)
+    return x
+
+# 디코더 구현
+import copy
+def get_clones(module, N):
+  return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+class TransformerDecoder(nn.Module):
+  def __init__(self, decoder_layer, num_layers):
+    super().__init__()
+    self.layers = get_clones(decoder_layer, num_layers)
+    self.num_layers = num_layers
+  
+  def forward(self, tgt, src):
+    output = tgt
+    for mod in self.layers:
+      output = mod(tgt, src)
+    return output
